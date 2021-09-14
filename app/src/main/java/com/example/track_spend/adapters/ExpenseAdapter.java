c329a4221be794ed8192b4c232e4cef51c48918f
Ian Oliwa <ian.oliwa@zeraki.co.ke>
2021-09-14 09:57:00 +0300

    Spacing for expense details and minor fix for show expenses drop down

--- app/src/main/java/com/example/track_spend/adapters/ExpenseAdapter.java
+++ app/src/main/java/com/example/track_spend/adapters/ExpenseAdapter.java
@@ -1,13 +1,11 @@
 package com.example.track_spend.adapters;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.track_spend.R;
